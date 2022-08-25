0e93a7f8bb643404b836d5909ff08b229d6a11a0
Mahfuzur Rahman <m.rahman@ramom.de>
2022-08-25 10:49:18 +0200

    added locustFile

---
+++ locustfile.py
@@ -0,0 +1,53 @@
+from locust import HttpUser, between, task
+
+class WebsiteUser(HttpUser):
+    host = "https://my-ml-app-service.azurewebsites.net:443"
+    wait_time = between(5, 15)
+
+    @task
+    def index(self):
+        self.client.get("/")
+
+    @task
+    def predict(self):
+        self.client.post("/predict", json={
+            "CRIM": {
+                "0":0.21124
+            },
+            "ZN":{
+                "0":12.5
+            },
+            "INDUS":{
+                "0":7.87
+            },
+            "CHAS":{
+                "0":0
+            },
+            "NOX":{
+                "0":0.524
+            },
+            "RM":{
+                "0":5.631
+            },
+            "AGE":{
+                "0":100.0
+            },
+            "DIS":{
+                "0":6.0821
+            },
+            "RAD":{
+                "0":5.0
+            },
+            "TAX":{
+                "0":311.0
+            },
+            "PTRATIO":{
+                "0":15.2
+            },
+            "B":{
+                "0":386.63
+            },
+            "LSTAT":{
+                "0":29.93
+            }
+        })
